20e818d8ad5fd8599be5f210ab027aba2c470696
ken-kaneki-9 <160580260+ken-kaneki-9@users.noreply.github.com>
2026-02-20 13:36:51 +0530

    Scanning the test.lox file

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+use std::env;
+use std::fs;
+fn main() {
+    let args:Vec<String>=env::args().collect();
+    if args.len()<3{
+      eprintln!("");
+      return;
+    }
+    let command=&args[1];
+    let filename=&args[2];
+    match command.as_str(){
+        "tokenize"=>{
+            let file_contents=fs::read_to_string(filename).unwrap_or_else(|_| {
+               eprintln!("Failed to read file {}", filename); 
+               String::new()
+            });
+            if !file_contents.is_empty(){
+                panic!("Scanner not implemented");
+            }else{
+                println!("EOF null");
+            }
+        }
+        _=>{
+            println!("Unknown command: {}", command);
+        }
+    }
+}
